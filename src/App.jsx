83c0f34c77cc8e7dc19fbd17de37976c42aa96a9
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2026-01-21 18:53:48 -0600

    added useRef to select

--- src/App.jsx
+++ src/App.jsx
@@ -1,184 +1,201 @@
-import { useState, useEffect } from 'react'
-import { main as getDailySpending } from './modules/ynab/index.js'
-import { BarChart } from '@mui/x-charts/BarChart'
-import HomeIcon from '@mui/icons-material/Home'
+import { useState, useEffect, useRef } from "react";
+import { main as getDailySpending } from "./modules/ynab/index.js";
+import { BarChart } from "@mui/x-charts/BarChart";
+import HomeIcon from "@mui/icons-material/Home";
 
 function DailyChart({ day, dailyTransactions }) {
-  console.log(Object.keys(dailyTransactions))
-  console.log('Object values', Object.values(dailyTransactions))
   const total = Object.values(dailyTransactions)
     .reduce((acc, current) => {
-      return acc += current
+      return (acc += current);
     }, 0)
-    .toFixed(2)
+    .toFixed(2);
   return (
     <div>
       <h2>Daily spending for {day}</h2>
       <div>Total spending: ${total}</div>
       <BarChart
         xAxis={[{ data: Object.keys(dailyTransactions) }]}
-        series={[{
-          data:
-            Object.values(dailyTransactions)
-              .sort((a, b) => b > a)
-              .map(i => i.toFixed(2)),
-          barLabel: "value"
-        }]}
+        series={[
+          {
+            data: Object.values(dailyTransactions)
+              .sort((a, b) => new Date(b) - new Date(a))
+              .map((i) => i.toFixed(2)),
+            barLabel: "value",
+          },
+        ]}
         height={500}
         layout="vertical"
       />
     </div>
-  )
+  );
 }
 
-function HomeButton({setIsHome}){
+function HomeButton({ setIsHome }) {
   return (
-  <div>
-      <button 
+    <div>
+      <button
         style={{
           padding: 5,
-          display: 'flex',
-          alignItems: 'center',
-          gap: 1
+          display: "flex",
+          alignItems: "center",
+          gap: 1,
         }}
-        onClick={() => setIsHome(true)
-        }><HomeIcon sx={{ fontSize: 14 }}/> return home</button>
+        onClick={() => setIsHome(true)}
+      >
+        <HomeIcon sx={{ fontSize: 14 }} /> return home
+      </button>
     </div>
-
-  )
+  );
 }
 
 function SelectAndDisplay({ range, setRange, setIsHome }) {
-  const [dailySpendingMap, setDailySpendingMap] = useState(null)
-  const [dates, setDates] = useState([])
-  const [day, setDay] = useState()
-  const [dailyTransactions, setDailyTransactions] = useState(null)
-  const [isLoading,setIsLoading] = useState(true)
-  const { start, end } = range
+  const [dailySpendingMap, setDailySpendingMap] = useState(null);
+  const [dates, setDates] = useState([]);
+  const [day, setDay] = useState(null);
+  const [dailyTransactions, setDailyTransactions] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const selectRef = useRef(null);
+  const { start, end } = range;
 
   function getDailyTotal(dailyTransaction) {
-    const [date, values] = Array.from(dailyTransaction)
-    const total = Object.values(values).reduce((acc, current) => acc += current, 0)
-    return `${date} - ${total.toFixed(2)}`
+    const [date, values] = Array.from(dailyTransaction);
+    const total = Object.values(values).reduce(
+      (acc, current) => (acc += current),
+      0,
+    );
+    return `${date} - ${total.toFixed(2)}`;
   }
   useEffect(() => {
     getDailySpending(range.start, range.end)
-      .then(res => {
-        setDailySpendingMap(res)
-        const datesWithTotals = Array.from(res.entries()).map(i => getDailyTotal(i))
-        setDates(datesWithTotals)
+      .then((res) => {
+        setDailySpendingMap(res);
+        const datesWithTotals = Array.from(res.entries()).map((i) =>
+          getDailyTotal(i),
+        );
+        setDates(datesWithTotals);
       })
-      .catch(e => {
-        console.log(`Error: ${e.message}`)
+      .catch((e) => {
+        cansole.log(`Error: ${e.message}`);
       })
       .finally(() => {
-        setIsLoading(false)
-      })
-  }, [])
+        setIsLoading(false);
+      });
+  }, [range]);
 
   function handleDaySelection(event) {
-    event.preventDefault()
-    const arr = event.target.value.split("-").map(i => i.trim())
-    const year = arr[0]
-    const month = arr[1]
-    const day = arr[2]
-    const date = `${year}-${month}-${day}`
-    setDay(date)
-    setDailyTransactions(dailySpendingMap.get(date))
+    event.preventDefault();
+    const arr = event.target.value.split("-").map((i) => i.trim());
+    const year = arr[0];
+    const month = arr[1];
+    const day = arr[2];
+    const date = `${year}-${month}-${day}`;
+    setDay(date);
+    setDailyTransactions(dailySpendingMap.get(date));
   }
 
-  if(isLoading){
-    return <div>Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   if (!dailySpendingMap || dates.length === 0) {
     return (
       <div>
-      <p>Nothing to display</p>
-       <HomeButton setIsHome={setIsHome}/> 
+        <p>Nothing to display</p>
+        <HomeButton setIsHome={setIsHome} />
       </div>
-    )
+    );
+  }
+
+  function resetSelect() {
+    if (selectRef.current) {
+      selectRef.current.value = "no date selected";
+      setDay(null);
+      setDailyTransactions(null);
+    }
   }
 
   return (
     <div>
-      <HomeButton setIsHome={setIsHome}/>
-      <p>You have selected to see the date range between {start} and {end}:</p>
+      <HomeButton setIsHome={setIsHome} />
+      <p>
+        You have selected to see the date range between {start} and {end}:
+      </p>
       <p>Select which day you would like to see:</p>
-       <div>
-        <select
-          onChange={handleDaySelection}
-        >
+      <div>
+        <select ref={selectRef} onChange={handleDaySelection}>
           <option>no date selected</option>
-          {dates.map(d =>
+          {dates.map((d) => (
             <option key={d}> {d}</option>
-          )}
-        </select >
-        <button onClick={() => setRange({ start: '', end: '' })} >clear</button>
+          ))}
+        </select>
+        {day ? (
+          <button style={{ leftPadding: 2 }} onClick={resetSelect}>
+            clear
+          </button>
+        ) : null}
       </div>
-      {
-        day && dailyTransactions ? <DailyChart
-          day={day}
-          dailyTransactions={dailyTransactions}
-        /> : null
-      }
-    </div >
-  )
+      {day && dailyTransactions ? (
+        <DailyChart day={day} dailyTransactions={dailyTransactions} />
+      ) : null}
+    </div>
+  );
 }
 
 function SelectRange({ setRange, setIsHome }) {
-  const [start, setStart] = useState('')
-  const [end, setEnd] = useState('')
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
   function handleSubmit(event) {
-    event.preventDefault()
-    setRange({ start: start, end: end })
-    setIsHome(false)
+    event.preventDefault();
+    setRange({ start: start, end: end });
+    setIsHome(false);
   }
   return (
     <div>
+      <p>
+        Welcome to my utility application to see how you much you have been
+        spending on daily basiWelcome to my utility application to see how you
+        much you have been spending on daily basis.
+      </p>
       <p>Select the start and end date of the range:</p>
       <form onSubmit={handleSubmit}>
         <div>
           <label>Start </label>
           <input
-            type='date'
+            type="date"
             onChange={({ target }) => setStart(target.value)}
           />
         </div>
         <div>
           <label>End </label>
-          <input
-            type="date"
-            onChange={({ target }) => setEnd(target.value)}
-          />
+          <input type="date" onChange={({ target }) => setEnd(target.value)} />
         </div>
         <button>select</button>
       </form>
     </div>
-  )
+  );
 }
 function App() {
-  const [isHome,setIsHome] = useState(true)
+  const [isHome, setIsHome] = useState(true);
   const [range, setRange] = useState({
-    start: '',
-    end: ''
-  })
+    start: "",
+    end: "",
+  });
 
   return (
     <>
-      { isHome ?
-        <SelectRange 
-          setRange={setRange}
-          setIsHome={setIsHome}
-        /> :
+      <h1>Daily Spending Report</h1>
+
+      {isHome ? (
+        <SelectRange setRange={setRange} setIsHome={setIsHome} />
+      ) : (
         <SelectAndDisplay
           range={range}
           setRange={setRange}
           setIsHome={setIsHome}
         />
-      }
+      )}
     </>
-  )
+  );
 }
 
 export default App;
--- src/modules/ynab/ynabApi.js
+++ src/modules/ynab/ynabApi.js
@@ -1,92 +1,95 @@
-import * as ynab from 'ynab'
+import * as ynab from "ynab";
 
 async function getBudgets(api) {
   try {
-
-    const budgetsResponse = await api
-      .budgets
-      .getBudgets()
-
-    console.log('budgetsResponse', budgetsResponse)
-
-    return budgetsResponse
-      .data
-      .budgets
+    const budgetsResponse = await api.budgets.getBudgets();
+    return budgetsResponse.data.budgets;
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
 }
 
 async function getAccounts(api, budgetId) {
   try {
-    const transactionResponse = await api
-      .accounts
-      .getAccounts(budgetId)
+    const transactionResponse = await api.accounts.getAccounts(budgetId);
 
-    return transactionResponse
-      .data
-      .accounts
+    return transactionResponse.data.accounts;
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
 }
 
 function checkForDueDatesAndRemove(category) {
   if (category.includes("-")) {
-    return category.split("-")[0].trim()
+    return category.split("-")[0].trim();
   }
-  return category
+  return category;
 }
 
 function cleanCategory(category) {
-  const sansEmoji = category.replaceAll(/\p{Emoji}/gu, '')
-  return checkForDueDatesAndRemove(sansEmoji)
+  const sansEmoji = category.replaceAll(/\p{Emoji}/gu, "");
+  return checkForDueDatesAndRemove(sansEmoji);
 }
 
 function simplifyTransactions(transactions) {
-  return transactions.map(t => {
-    const { id, date, amount, account_name, payee_name, category_name } = t
-    const amountConverted = ynab.utils.convertMilliUnitsToCurrencyAmount(amount, 2)
+  return transactions.map((t) => {
+    const { id, date, amount, account_name, payee_name, category_name } = t;
+    const amountConverted = ynab.utils.convertMilliUnitsToCurrencyAmount(
+      amount,
+      2,
+    );
     return {
       id,
       date,
       amount: amountConverted,
       account_name,
       payee_name,
-      category_name: cleanCategory(category_name)
-    }
-  })
+      category_name: cleanCategory(category_name),
+    };
+  });
 }
 
 async function getTransactionsByAccount(api, budgetId, accountId) {
   try {
-    const transactionResponse = await api
-      .transactions
-      .getTransactionsByAccount(budgetId, accountId)
+    const transactionResponse = await api.transactions.getTransactionsByAccount(
+      budgetId,
+      accountId,
+    );
 
-    return transactionResponse
-      .data
-      .transactions
+    return transactionResponse.data.transactions;
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
 }
 
 export function filterTransactionsByRange(transactions, startDate, endDate) {
-  return transactions.filter(t => t.date >= startDate && t.date <= endDate)
+  return transactions.filter((t) => t.date >= startDate && t.date <= endDate);
 }
 
-async function getAndTransformTransactions(api, budgetId, accountId, startDate, endDate) {
+async function getAndTransformTransactions(
+  api,
+  budgetId,
+  accountId,
+  startDate,
+  endDate,
+) {
   try {
-    const transactions = await getTransactionsByAccount(api, budgetId, accountId)
-    const filteredTransactions = filterTransactionsByRange(transactions, startDate, endDate)
-    return simplifyTransactions(filteredTransactions)
+    const transactions = await getTransactionsByAccount(
+      api,
+      budgetId,
+      accountId,
+    );
+    const filteredTransactions = filterTransactionsByRange(
+      transactions,
+      startDate,
+      endDate,
+    );
+    return simplifyTransactions(filteredTransactions);
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
 }
 
-
 export default {
-  getAndTransformTransactions
-}
+  getAndTransformTransactions,
+};
