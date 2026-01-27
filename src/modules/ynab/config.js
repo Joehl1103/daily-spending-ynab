9825aaf108040d6423324a3c0b1dd0ba7bb9cc01
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2026-01-27 07:47:10 -0600

    modified config and added personal and test .env

--- src/modules/ynab/config.js
+++ src/modules/ynab/config.js
@@ -1,15 +1,6 @@
 const configs = {
-  personal: {
-    budgetId: import.meta.env.VITE_MAIN_BUDGET_ID,
-    accountId: import.meta.env.VITE_MAIN_CHECKING_ID,
-  },
-  test: {
-    budgetId: import.meta.env.VITE_TEST_BUDGET_ID,
-    accountId: import.meta.env.VITE_TEST_BUDGET_CHECKING_ID,
-  },
+  budgetId: import.meta.env.VITE_MAIN_BUDGET_ID,
+  accountId: import.meta.env.VITE_MAIN_CHECKING_ID,
 };
 
-const mode = import.meta.env.VITE_MODE || "test";
-
-export const getConfig = () => configs[mode];
-export const currentMode = mode;
+export const getConfig = () => configs;
